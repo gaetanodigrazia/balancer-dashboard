07ff77a1e43009765c6bba293d2ed117887cf725
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2025-06-21 01:09:36 +0200

    improved mobile table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   formData: any = {};
+  sidebarOpen: boolean = false;
+  isMobileView: boolean = false;
 
-  updateData(partial: any) {
+  ngOnInit(): void {
+    this.checkWindowSize();
+    window.addEventListener('resize', this.checkWindowSize.bind(this));
+  }
+
+  updateData(partial: any): void {
     this.formData = { ...this.formData, ...partial };
+  }
+
+  checkWindowSize(): void {
+    this.isMobileView = window.innerWidth < 768;
+  }
+
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+    document.body.classList.toggle('sidebar-open', this.sidebarOpen);
   }
 }
